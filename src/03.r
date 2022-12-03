9a0b94e0515daddcce5875f812b5e6a71080ce48
Mateusz Sienkan <sin.mateusz@gmail.com>
2022-12-03 20:34:41 +0100

    Add day 3

---
+++ src/03.r
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+data <- readLines("data/03.txt") %>%
+  tibble(rucksack = .)
+
+find_common_items <- function(data) {
+  data |>
+    mutate(items = strsplit(rucksack, "")) |>
+    mutate(sacks = map(items, ~ split(., cut(seq_along(.), 2)))) |>
+    transmute(common = map_chr(sacks, ~ Reduce(intersect, .)))
+}
+
+find_group_badges <- function(data) {
+  data |>
+    mutate(items = strsplit(rucksack, "")) |>
+    group_by(group = gl(n(), 3, n())) |>
+    summarize(common = Reduce(intersect, as.list(items)))
+}
+
+calculate_priority <- function(common_items) {
+  common_items |>
+    mutate(code = sapply(common, utf8ToInt)) |>
+    mutate(priority = code - ifelse(code > 96, 96, 38)) |>
+    summarize(sum(priority))
+}
+
+data |>
+  find_common_items() |>
+  calculate_priority()
+
+data |>
+  find_group_badges() |>
+  calculate_priority()
