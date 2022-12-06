bb742c6b9cac438075700c8b353b288c9da3abaa
Mateusz Sienkan <sin.mateusz@gmail.com>
2022-12-06 09:17:53 +0100

    Update day 1

--- src/01.r
+++ src/01.r
@@ -1,14 +1,7 @@
 library(tidyverse)
 
-snacks <- readLines("data/01.txt") %>%
-  tibble(calories = .) %>%
-  group_by(elf = cumsum(calories == "")) %>%
-  filter(calories != "") %>%
-  summarize(calories = sum(as.integer(calories)))
-
-snacks %>%
-  top_n(1, calories)
-
-snacks %>%
-  top_n(3, calories) %>%
-  summarize(sum(calories))
+read_lines("data/01.txt") %>%
+  tibble(calories = ., elf = cumsum(calories == "")) |>
+  count(elf, wt = as.numeric(calories)) |>
+  top_n(3) |>
+  summarize(top1 = max(n), top3 = sum(n))
